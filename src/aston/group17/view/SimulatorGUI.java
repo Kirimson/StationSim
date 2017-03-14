205b2b1d79af910ba93df15fca7ef679ce757f82
Kirimson <kierangates@outlook.com>
2017-03-14 18:33:03 +0000

    Changes to simualtor and added to simulatorGUI

--- src/aston/group17/view/SimulatorGUI.java
+++ src/aston/group17/view/SimulatorGUI.java
@@ -1,5 +1,32 @@
 package aston.group17.view;
+import aston.group17.simulator.*;
 
-public class SimulatorGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class SimulatorGUI extends JFrame {
+	private JFrame mainFrame;
+	private Simulator s;
+	
+	public SimulatorGUI()
+	{
+		// Step 1: create the components
+		JLabel pump1Label = new JLabel("Pump 1: ");
+		// Step 2: Set the properties of the components
+		
+		// Step 3: Create containers to hold the components
+		mainFrame = new JFrame("StationSim");
+		
+		// Step 4: Specify LayoutManagers
+		
+		// Step 5: Add components to containers
+		
+		// Step 6: Arrange to handle events in the user interface
+		
+		// Step 7: Display the GUI
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		
+	}
 
 }
