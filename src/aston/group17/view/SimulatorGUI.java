a95fb75e21e9f3a3dda6e350884bd7554eccdc6c
MitchFeaver <mitch_feaver@hotmail.co.uk>
2017-04-04 00:09:13 +0100

    GUI additions

--- src/aston/group17/view/SimulatorGUI.java
+++ src/aston/group17/view/SimulatorGUI.java
@@ -8,20 +8,113 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class SimulatorGUI {
+	
 	private JFrame menuFrame;
 	private Simulator s;
-	private LabelledSlider pSlider; 
-	private LabelledSlider qSlider;
-	private LabelledSlider priceSlider;
-	private JTextField periodTime;
 	private JTextArea log;
 	private Simulator simulator;
 	
 	public SimulatorGUI() {
 		
+		JFrame firstFrame;
+			
+			final int blankSpace = 6;  // blank at edge of panels
+
+//		 Step 1: create the components
+			JButton startButton = new JButton();
+			JButton resetButton = new JButton();
+			JButton quitButton = new JButton();
+			final JLabel titleLabel = new JLabel("Group 17. Kieran Gates, Mitchell Feaver, Zak, Harleen and Mo");
+			
+			log = new JTextArea();
+			log.setEditable(false);
+			JScrollPane actionList = new JScrollPane(log);
+			actionList.setPreferredSize(new Dimension(300, 300));
+			actionList.setMinimumSize(new Dimension(200,200));
+			
+//		 Step 2: Set the properties of the components
+			startButton.setText("Start");
+			startButton.setToolTipText("Start the simulation");
+			resetButton.setText("Reset");
+			resetButton.setToolTipText("Reset the simulation");
+			quitButton.setText("Quit");
+			quitButton.setToolTipText("Quit application");
+			
+//		 Step 3: Create containers to hold the components
+			firstFrame = new JFrame("Fuelling Station Simulator");
+			firstFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+			
+			JPanel buttons = new JPanel();
+			JPanel title = new JPanel();
+			
+//		 Step 4: Specify LayoutManagers
+			firstFrame.setLayout(new BorderLayout());
+			((JPanel)firstFrame.getContentPane()).setBorder(new 
+					EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+			
+			buttons.setLayout(new FlowLayout());
+			buttons.setBorder(new 
+					EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+					
+			log.setLayout(new FlowLayout());
+			log.setBorder(new 
+					EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+					
+			title.setLayout(new FlowLayout());
+			title.setBorder(new 
+					EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+			
+//		 Step 5: Add components to containers 
+			buttons.add(startButton);
+			buttons.add(resetButton);
+			buttons.add(quitButton);
+			firstFrame.add(buttons, BorderLayout.SOUTH);
+			firstFrame.add(actionList, BorderLayout.CENTER);
+			firstFrame.add(titleLabel, BorderLayout.NORTH);
+					
+//		 Step 6: Arrange to handle events in the user interface
+			
+			firstFrame.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+				exitApp();
+				}
+			});    
+				
+				quitButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				exitApp();
+				}
+			});
+				
+			resetButton.addActionListener(new ActionListener() { 
+				public void actionPerformed(ActionEvent e) {
+				resetApp();
+				}
+			});
+				
+			startButton.addActionListener(new ActionListener() { 
+				public void actionPerformed(ActionEvent e) {
+				setParameters();
+				}
+			});
+			
+//				Step 7: Display GUI
+				firstFrame.pack();
+				firstFrame.setVisible(true);
+	}
+	
+	private void resetApp(){
+		setParameters();
+	}
+	private void setParameters(){
+		
 		final int blankSpace = 6;  // blank at edge of panels
 
 
+		LabelledSlider pSlider; 
+		LabelledSlider qSlider;
+		LabelledSlider priceSlider;
+		JTextField periodTime;
 	
 		
 //	Step 1: create the components for the 
@@ -173,14 +266,6 @@ public class SimulatorGUI {
 		if (response == JOptionPane.YES_OPTION) {
 			System.exit(0);
 		}
-	}
-
-	private void resetApp(){
-		setParameters();
-		
-	}
-	private void setParameters(){
-		
 	}
 }
 
